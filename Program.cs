f5a70d73b7807a4e02193c16b9e2869eb66dd938
tunga76 <hikmettunga@gmail.com>
2024-06-23 19:41:18 +0300

    first Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+Log.Logger = new LoggerConfiguration().WriteTo.Seq(serverUrl:"http://localhost:5341",apiKey:"paJ2AWiukGytg2C7pfPl").CreateLogger();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
